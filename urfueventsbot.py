f4c75992aee29d89972746c564c724b092df3472
Alphiruwa <59170859+Alphiruwa@users.noreply.github.com>
2020-05-05 17:58:37 +0500

    Add files via upload

--- urfueventsbot.py
+++ urfueventsbot.py
@@ -4,20 +4,18 @@ import json
 
 from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
 
-vk_session = vk_api.VkApi(token='199645f330de1d079a2c0602dac55163c593fd9106d4873265c3b5f31221f4f58eef5ec0c6b6838a01e93', api_version=5.95)
-vk = vk_session.get_api()
-longpoll = VkBotLongPoll(vk_session, 192978319)
+vk = vk_api.VkApi(token='199645f330de1d079a2c0602dac55163c593fd9106d4873265c3b5f31221f4f58eef5ec0c6b6838a01e93', api_version=5.95)
 
 startbutton = 'Начать'
 startmessage0 = 'Прежде чем найти команду на мероприятия, расскажи немного о себе!'
 startmessage1 = 'Для начала введи свои фамилию, имя и отчество! Эти данные нужны для того, чтобы капитан команды мог записать тебя на мероприятие!'
 startmessage2 = 'Отлично, теперь назови свою академическую группу, например: "РИ-190012"!'
 startmessage3 = 'И последний шаг: назови своё направление и курс, например: "Программная Инженерия, 1 курс"'
-
 mainbutton0 = 'Найти команду'
 mainbutton1 = 'Организовать свою команду'
 mainbutton2 = 'Доступные мероприятия'
 mainbutton3 = 'Обновить информацию о себе'
+unknownmessage = 'Я плохо понимаю человеческий, давай действовать по инструкции!'
     
 def get_button(label, color, payload=""):
     return {
@@ -41,4 +39,19 @@ keyboard = {
 }
 
 while True:
-
+    messages = vk.method('messages.getConversations', {'offset':0, 'count':20, 'filter':'unread'})
+    if messages['count'] > 0:
+        id = messages['items'][0]['last_message']['from_id']
+        body = messages['items'][0]['last_message']['text']
+        if body.lower() == mainbutton0.lower():
+            vk.method('messages.send', {'peer_id':id, 'message':'*список команд с мероприятиями и капитанами*', 'random_id':''})            
+        elif body.lower() == mainbutton1.lower():
+            vk.method('messages.send', {'peer_id':id, 'message':'*заполнение информации о команде т.е. выбор мероприятия и требования к участникам*', 'random_id':''}) 
+        elif body.lower() == mainbutton2.lower():
+            vk.method('messages.send', {'peer_id':id, 'message':'*список мероприятий с датами*', 'random_id':''})              
+        elif body.lower() == mainbutton3.lower():
+            vk.method('messages.send', {'peer_id':id, 'message':'*снова заполнение данных со старта*', 'random_id':''})
+        else:
+            vk.method('messages.send', {'peer_id':id, 'message':unknownmessage, 'random_id':''})        
+    time.sleep(1)
+ 
